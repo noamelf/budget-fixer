c87f2a48a33a4e07fc4d5779cd164b462494efc9
Noam Elfanbaum <noam.elf@gmail.com>
2019-12-23 20:36:16 +0200

    Add poetry lock file

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+import datetime
 import logging
 import os
 from collections import defaultdict
 from difflib import SequenceMatcher
+from typing import Dict, Tuple, List
 
 import requests
 from dotenv import load_dotenv
 
 TOSHL_URL = 'https://api.toshl.com'
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format='[%(asctime)-15s]: %(message)s')
 logger = logging.getLogger()
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv('TOSHL_TOKEN')
 session = requests.Session()
 session.auth = (TOKEN, '')
 
@@ -22,8 +24,6 @@ def similar(a, b):
     logger.debug('Before split: %s, %s', a, b)
     a, b = a.split('\n')[0], b.split('\n')[0]
     similarity = SequenceMatcher(isjunk=None, a=a.split('\n')[0], b=b.split('\n')[0]).ratio()
-    logger.info('Items: %s, %s', a, b)
-    logger.info(f'Similarity ratio: {similarity}')
     return similarity
 
 
@@ -44,47 +44,63 @@ def delete_entry(_id):
 
 def get_entries(page=0):
     return session.get(f'{TOSHL_URL}/entries',
-                       params={
-                           'from': '2019-01-01',
-                           'to': '2019-10-31',
-                           'page': page
-                       })
+                       params={'from': '2018-01-01', 'to': str(datetime.date.today()), 'page': page})
 
 
-page = 0
-entries = []
+def get_all_entries():
+    page = 0
+    total_entries = []
+    while True:
+        result = get_entries(page)
+        total_entries.extend(result.json())
+        if 'next' not in result.links:
+            break
+        page += 1
 
-while True:
-    result = get_entries(page)
-    entries.extend(result.json())
-    if not result.links.get('next'):
-        break
+    entries_by_id = {entry['id']: entry for entry in total_entries}
+    return entries_by_id
 
-    page += 1
 
-comp_list = [(entry['id'], entry['amount'], entry['date']) for entry in entries]
-by_id = {entry['id']: entry for entry in entries}
+def find_duplicates(entries) -> List[List[Dict]]:
+    counter = defaultdict(list)
+    for _id, entry in entries.items():
+        entry_key = entry['amount'], entry['date']
+        existing_entry = counter.get(entry_key)
+        if existing_entry:
+            existing_entry_desc = existing_entry[0]['desc']
+            similarity = similar(existing_entry_desc, entry['desc'])
+            logger.info(f'For entry {entry["desc"]}.\n'
+                        f'Found existing one: {existing_entry_desc}.\n'
+                        f'Similarity score is: {similarity}')
+            if similarity > 0.7:
+                logger.info('Adding to duplicates')
+                counter[entry_key].append(entry)
+            else:
+                logger.info('Skipping')
+        else:
+            counter[entry_key].append(entry)
+    duplicates = [dup_entries for dup_entries in counter.values() if len(dup_entries) >= 2]
+    return duplicates
 
-d = defaultdict(list)
-for _id, amount, date in comp_list:
-    d[(amount, date)].append(_id)
 
-duplicates = [ids for entry, ids in d.items() if len(ids) >= 2]
+def delete_duplicates(entries):
+    duplicates = find_duplicates(entries)
+    for dup_entries in duplicates:
+        delete_entry(dup_entries[0]['id'])
 
-for ids in duplicates:
-    similarity = similar(by_id[ids[0]]['desc'], by_id[ids[1]]['desc'])
-    logger.info(f'Amount: {by_id[ids[0]]["amount"]}')
 
-    if similarity < 0.5:
-        logger.info('Same amount but similarity score low, skipping')
-    elif similarity > 0.7:
-        logger.info('High similarity - deleting')
-        delete_entry(ids[0])
-    else:
-        logger.info('Medium similarity score')
-        ask_to_delete(_id)
+def delete_visa(entries_by_id):
+    for _id, entry in entries_by_id.items():
+        if entry['desc'] == 'לאומי ויזה י':
+            logger.info(f'{entry["desc"]}, {entry["amount"]}')
+            ask_to_delete(_id)
 
-for id, entry in by_id.items():
-    if entry['desc'] == 'לאומי ויזה י':
-        logger.info(f'Can I delete? {entry["desc"]}, {entry["amount"]}')
-        ask_to_delete(entry['id'])
+
+def main():
+    entries: Dict[str, Dict] = get_all_entries()
+    delete_duplicates(entries)
+    delete_visa(entries)
+
+
+if __name__ == '__main__':
+    main()
