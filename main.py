4eb6835aaf1efc3336ea335d4dc859a5055ac201
Noam Elfanbaum <noam.elf@gmail.com>
2019-12-23 13:07:09 +0200

    Basic version working

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 
 TOSHL_URL = 'https://api.toshl.com'
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 load_dotenv()
@@ -18,18 +19,48 @@ session.auth = (TOKEN, '')
 
 
 def similar(a, b):
-    logger.debug('Before split:', a, b, end='\n')
+    logger.debug('Before split: %s, %s', a, b)
     a, b = a.split('\n')[0], b.split('\n')[0]
-    logger.debug('After split:', a, b, end='\n')
-    return SequenceMatcher(isjunk=None, a=a.split('\n')[0], b=b.split('\n')[0]).ratio()
+    similarity = SequenceMatcher(isjunk=None, a=a.split('\n')[0], b=b.split('\n')[0]).ratio()
+    logger.info('Items: %s, %s', a, b)
+    logger.info(f'Similarity ratio: {similarity}')
+    return similarity
 
 
-result = session.get(f'{TOSHL_URL}/entries',
-                     params={
-                         'from': '2019-11-01',
-                         'to': '2019-11-30'
-                     })
-entries = result.json()
+def ask_to_delete(_id):
+    logger.handlers[0].flush()
+    answer = input('Should I delete (y / n)')
+    if answer == 'y':
+        delete_entry(_id)
+    else:
+        logger.info('skipping')
+
+
+def delete_entry(_id):
+    url = f'{TOSHL_URL}/entries/{_id}'
+    logger.debug(url)
+    session.delete(url)
+
+
+def get_entries(page=0):
+    return session.get(f'{TOSHL_URL}/entries',
+                       params={
+                           'from': '2019-01-01',
+                           'to': '2019-10-31',
+                           'page': page
+                       })
+
+
+page = 0
+entries = []
+
+while True:
+    result = get_entries(page)
+    entries.extend(result.json())
+    if not result.links.get('next'):
+        break
+
+    page += 1
 
 comp_list = [(entry['id'], entry['amount'], entry['date']) for entry in entries]
 by_id = {entry['id']: entry for entry in entries}
@@ -41,12 +72,19 @@ for _id, amount, date in comp_list:
 duplicates = [ids for entry, ids in d.items() if len(ids) >= 2]
 
 for ids in duplicates:
-    logger.debug(ids)
-    logger.debug(by_id[ids[0]]['amount'], by_id[ids[0]]['desc'])
-    logger.debug(by_id[ids[0]]['amount'], by_id[ids[1]]['desc'])
-    logger.debug(similar(by_id[ids[0]]['desc'], by_id[ids[1]]['desc']))
+    similarity = similar(by_id[ids[0]]['desc'], by_id[ids[1]]['desc'])
+    logger.info(f'Amount: {by_id[ids[0]]["amount"]}')
 
-for ids in duplicates:
-    url = f'{TOSHL_URL}/entries/{ids[1]}'
-    logger.debug(url)
-    session.delete(url)
+    if similarity < 0.5:
+        logger.info('Same amount but similarity score low, skipping')
+    elif similarity > 0.7:
+        logger.info('High similarity - deleting')
+        delete_entry(ids[0])
+    else:
+        logger.info('Medium similarity score')
+        ask_to_delete(_id)
+
+for id, entry in by_id.items():
+    if entry['desc'] == 'לאומי ויזה י':
+        logger.info(f'Can I delete? {entry["desc"]}, {entry["amount"]}')
+        ask_to_delete(entry['id'])
