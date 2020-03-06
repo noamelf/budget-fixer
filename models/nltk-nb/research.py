8ee8ba1f40116abf4fe4a8d31934b67766ddd2a0
Noam Elfanbaum <noam.elf@gmail.com>
2020-03-06 20:25:18 +0200

    Arrange ml model code

--- models/nltk-nb/research.py
+++ models/nltk-nb/research.py
@@ -1,16 +1,16 @@
 # %%
+import os
+import shutil
+
 import mlflow.pyfunc
 import nltk
 import pandas as pd
 import sys
 from sklearn.model_selection import train_test_split
 
-data_file = sys.argv[1] or '/home/noam/code/toshl-fixer/toshl_fixer/db/expenses.csv'
-# %%
+data_file = sys.argv[1] or '../toshl_fixer/db/expenses.csv'
 expenses = pd.read_csv(data_file)
-# expenses = get_expenses()
 
-# %%
 expenses['label'] = expenses['category'] + '/' + expenses['tag']
 expenses['desc'] = expenses['desc'].str.strip('🏷')
 expenses = expenses[expenses['amount'] < 0]
@@ -19,11 +19,8 @@ data_set = expenses[['desc', 'label']]
 data_set = data_set.astype('str')
 data_set = data_set.dropna()
 
-# %%
 train, test = train_test_split(data_set, test_size=0.2)
 
-
-# %%
 
 def format_for_nltk(row):
     return {word: True for word in row["desc"].upper().split()}
@@ -38,31 +35,33 @@ test_y = test["label"]
 train_set = list(zip(train_x, train_y))
 test_set = list(zip(test_x, test_y))
 
-# %%
+
+class ExpensesClassifier(mlflow.pyfunc.PythonModel):
+
+    def __init__(self, model):
+        self.model = model
+
+    def predict(self, context, model_input):
+        return model_input.apply(classifier.classify)
+
+
 with mlflow.start_run():
-    classifier = nltk.NaiveBayesClassifier.train(train_set)
-    accuracy = nltk.classify.accuracy(classifier, test_set)
-
-    print(f'{accuracy=}')
-    mlflow.log_metric("accuracy", accuracy)
-
     train.to_csv('train-set.csv', index=False)
-    mlflow.log_artifact('train-set.csv')
-
     test.to_csv('test-set.csv', index=False)
+    mlflow.log_artifact('train-set.csv')
     mlflow.log_artifact('test-set.csv')
 
+    classifier = nltk.NaiveBayesClassifier.train(train_set)
 
-    class ExpensesClassifier(mlflow.pyfunc.PythonModel):
-
-        def __init__(self, model):
-            self.model = model
-
-        def predict(self, context, model_input):
-            return model_input.apply(classifier.classify)
-
+    accuracy = nltk.classify.accuracy(classifier, test_set)
+    print(f'{accuracy=}')
+    mlflow.log_metric("accuracy", accuracy)
 
     expenses_classifier_path = "expenses_classifier"
     expenses_classifier_model = ExpensesClassifier(classifier)
     mlflow.pyfunc.save_model(path=expenses_classifier_path, python_model=expenses_classifier_model)
-    mlflow.pyfunc.log_model(expenses_classifier_path, expenses_classifier_model)
+    mlflow.pyfunc.log_model(artifact_path=expenses_classifier_path, python_model=expenses_classifier_model)
+
+shutil.rmtree('expenses_classifier')
+os.remove('test-set.csv')
+os.remove('train-set.csv')
